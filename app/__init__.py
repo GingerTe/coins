745bf543f7883f839cfb3dced42a960ecd28e323
Maria Prudyvus <maria.prudyvus@gmail.com>
2018-08-12 21:58:05 +0300

    users auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app(config_name):
@@ -14,9 +16,13 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
     #
     # from .auth import auth as auth_blueprint
     # app.register_blueprint(auth_blueprint, url_prefix='/auth')
